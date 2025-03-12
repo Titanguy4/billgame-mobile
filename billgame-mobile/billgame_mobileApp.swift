7ee4328ed90bbe658708a63ece5ecce5b2902485
KyZiiKs <86744162+Titanguy4@users.noreply.github.com>
2025-03-12 08:10:42 +0100

    Initial Commit

---
+++ billgame-mobile/billgame_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  billgame_mobileApp.swift
+//  billgame-mobile
+//
+//  Created by Hugo Tanguy on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct billgame_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
